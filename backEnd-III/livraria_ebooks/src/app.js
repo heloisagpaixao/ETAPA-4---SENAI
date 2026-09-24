ddb32d4de1c29eb849f6f3a7f9a78bd463b0b506
heloísa <heloisa.paixao@aluno.senai.br>
2026-09-24 07:35:50 -0300

    api livraria - exercicio

---
+++ backEnd-III/livraria_ebooks/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const routes = require('./routes');
+
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger_output.json");
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+// Registro de rotas
+app.use(routes);
+
+module.exports = app;
